180915264bd01fba229e63d2786f7f9656216036
SysoievB <sysoiev.bogdan.ua@gmail.com>
2023-10-17 07:05:08 +0200

    Added streams task

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,20 +1,21 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
+
+import static io.javabrains.reactiveworkshop.ReactiveSources.intNumbersFlux;
 
 public class Exercise3 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumbersFlux()
-
-        // Get all numbers in the ReactiveSources.intNumbersFlux stream
-        // into a List and print the list and its size
-        // TODO: Write code here
-
+        System.out.println("Get all numbers in the ReactiveSources.intNumbersFlux stream" +
+                "\ninto a List and print the list and its size");
+        List<Integer> list = intNumbersFlux().toStream().toList();
+        list.forEach(values -> System.out.println("The list is: " + values.toString()));
+        System.out.println("The size of the list is: " + list.size());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
-
 }
